d4563ba22f1baae1ab88b1dea911fc54bafe03a5
Rabin <rabindc77@gmail.com>
2026-08-07 15:42:37 +0545

    this is js

---
+++ HTML/java.js
@@ -0,0 +1,8 @@
+ <script>
+        const title = document.getElementById("title");
+        const button = document.getElementById("changeButton");
+
+        button.addEventListener("click", function() {
+            title.innerText = "JavaScript is awesome!";
+        });
+    </script>
