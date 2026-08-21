e65144272d876925ceecdc109fbf0f1e17d96491
Rabin <rabindc77@gmail.com>
2026-08-22 00:45:23 +0545

    update submodules and dom files

--- HTML/java.js
+++
@@ -1,8 +0,0 @@
- <script>
-        const title = document.getElementById("title");
-        const button = document.getElementById("changeButton");
-
-        button.addEventListener("click", function() {
-            title.innerText = "JavaScript is awesome!";
-        });
-    </script>
